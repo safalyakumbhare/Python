47be633e945a7814f4343caf427fbbe70889d935
safalyakumbhare <safalyakumbhare@gmail.com>
2023-12-20 20:49:08 +0530

    Arithmatic Operator

---
+++ Operators.py
@@ -0,0 +1,6 @@
+a = 2;
+b= 4;
+print("This Addition of two number i.e a and b",a+b) #Addition Operator
+print("This is the Subtraction of two number",a-b) #Subtraction
+print("This is the Multiplication of two number",a*b) #Multiplication
+print("This is the Division of two number",a/b) #Division
