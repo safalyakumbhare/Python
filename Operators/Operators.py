754d31ec2b120bb06622affb3f22a4bb91a240e5
safalyakumbhare <safalyakumbhare@gmail.com>
2023-12-21 20:47:12 +0530

    Operator

---
+++ Operators/Operators.py
@@ -0,0 +1,7 @@
+a = 2;
+b = 4;
+print("This is the Addition of two number i.e a and b",a+b) #Addition Operator
+print("This is the Subtraction of two number",a-b) #Subtraction
+print("This is the Multiplication of two number",a*b) #Multiplication
+print("This is the Division of two number",a/b) #Division
+print("This is the Modulation of two number",a%b)#Molulation
