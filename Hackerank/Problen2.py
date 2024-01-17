bfb2019c97d6d87f6da725037727faba7cac45fe
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-01-17 20:41:31 +0530

    Problem 1

---
+++ Hackerank/Problen2.py
@@ -0,0 +1,20 @@
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+if(n % 2 !=0):
+    print("Werid")
+elif n % 2 == 0 and n in range(2,5):
+    print("Not Werid")
+elif n % 2 == 0 and n in range(6,20):
+    print("Werid")
+else:
+    print("Not Weird")
