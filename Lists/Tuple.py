6ce3c2d696929614997901e80342aeff2a278724
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-01-21 23:45:41 +0530

    Lists

--- Lists/Tuple.py
+++ Lists/Tuple.py
@@ -1,3 +1,3 @@
-# It is also a type of list which is  amutable or not allow to modify the data.
+# It is also a type of list which is  imutable or not allow to modify the data.
 gender = ("Male", "Female", "Trans")
 print(gender)
