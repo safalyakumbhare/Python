9cedcab6bf5bf138363de92503b75de104aad811
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-01-25 19:19:17 +0530

    Strings

---
+++ String.py
@@ -0,0 +1,9 @@
+s1 = 'Hello There' #This is a single quotes String
+s2 = "I am a Double Quotes String" #This is a Double Quotes String
+s3 = '''Hey! 
+I am 
+a 
+Multiline String ,
+Just use triple Quotes to use me''' 
+print(s3)
+print(type(s3))
