6eb3d7c866b0eacebbe2b9529306488cd49dc49a
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-01-29 23:08:04 +0530

    result

--- result.py
+++ result.py
@@ -1,12 +1,13 @@
-marks1 = int(input("Enter the Marks of Subject 1 "))
-marks2 = int(input("Enter the Marks of Subject 2 "))
-marks3 = int(input("Enter the Marks of Subject 3 "))
-per = (marks1 + marks2 + marks3) /3
-if(per>=80):
+marks1 = int(input("Enter the Marks of Subject 1 :"))
+marks2 = int(input("Enter the Marks of Subject 2 :"))
+marks3 = int(input("Enter the Marks of Subject 3 :"))
+per = (marks1 + marks2 + marks3) / 3
+print("The Total Percentage is :", per)
+if per >= 80:
     print("Grade : A")
-elif(per>=70 and per<80):
+elif per >= 70 and per < 80:
     print("Grade : B")
-elif(per>=60 and per<70):
+elif per >= 60 and per < 70:
     print("Grade : C")
 else:
     print("Grade :D")
