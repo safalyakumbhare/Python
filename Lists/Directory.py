527428d213e2e55626184927df125f8c87ec7711
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-01-31 20:18:28 +0530

    dictionary

--- Lists/Directory.py
+++ Lists/Directory.py
@@ -1,3 +1,5 @@
+#It is also a type of list in python which stores more than one values in a single variable .
+# in dictionary we can assign a key to a element , by using the key we can access the the  value associated with that particular key. 
 d = {'BA' :'Bhagyshree Ambulkar','DS' : 'Dhiraj Shembekar','RI':'Rekha Israni'}
-print(d['BA'])
-print(d.keys())
+print(d['BA'])  #Accessing  the value by using key name i.e., BA, DS etc..
+print(d.keys()) #Accessing  all keys present inside dictionary 
