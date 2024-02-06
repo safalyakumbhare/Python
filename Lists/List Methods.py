2f92af98152c530f54e7085e9fae69d08f1621c2
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-06 19:50:08 +0530

    List Methods

--- Lists/List Methods.py
+++ Lists/List Methods.py
@@ -1,8 +1,10 @@
+# To know each method ,just uncomment and perform its operation
 num = [4,8,7,2,9,1,5,3,6,10]
-num.append(69)  # Adds New items at the end of the list
-num.sort()  # Sorting the List in Ascending Order
+# num.append(69)  # Adds New items at the end of the list
+# num.sort()  # Sorting the List in Ascending Order
 # num.sort(reverse=True)
-
-
+# num.reverse()  # Reverses the order of the elements in a list
+# num.insert(3,999) # Inserts an item to any position in the list
+# num.remove(7)    # Removes the specified value from the list
 for i in num:
     print(i)
