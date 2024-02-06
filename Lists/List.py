867fb6fcf371712f3ed2952cc8b7af8fa7735400
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-06 19:33:47 +0530

    List

--- Lists/List.py
+++ Lists/List.py
@@ -1,4 +1,15 @@
-#List in python is used to add various types of data in a single varible, we can perform multiple operations in lists such as append,delete,modify the data.
-name = ["Abhay","Adi","Veena","Yash"]
-name.append("Bhuavn")
-print(name) 
+# List in python is used to add various types of data in a single varible, we can perform multiple operations in lists such as append,delete,modify the data.
+name = [
+    "Veena",
+    "Yash",
+    "Kajal",
+    "Utkarsh",
+    "Nitish",
+    "Akanksha",
+    "Sakshi",
+    "Rahul",
+    "Abhay",
+    "Adi",
+]
+
+print(name)
