606d187c2fe2b940b778c5f8cd4fdbdfc83a4682
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-06 19:34:01 +0530

    List Methods

---
+++ Lists/List Methods.py
@@ -0,0 +1,8 @@
+num = [4,8,7,2,9,1,5,3,6,10]
+num.append(69)  # Adds New items at the end of the list
+num.sort()  # Sorting the List in Ascending Order
+# num.sort(reverse=True)
+
+
+for i in num:
+    print(i)
