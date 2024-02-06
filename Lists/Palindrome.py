dc9fa9b4f81e94becf26724f133675bbfb407a90
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-06 22:38:58 +0530

    Check Palindrome

---
+++ Lists/Palindrome.py
@@ -0,0 +1,13 @@
+li = []
+for i in range (5):
+    li.append(input("Enter the Number to check whether it is a Palindrome or not : "))
+
+print ("The list of numbers entered by the user are:",li)
+lic = li.copy()
+
+li.reverse()
+
+if lic == li:
+    print("It is a Palindrome")
+else:
+    print("It is not a Palindrome") 
