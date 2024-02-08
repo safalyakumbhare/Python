d579053a3bd0805d69f950861869045fcca4c838
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-08 22:47:58 +0530

    Dictionary List

--- Lists/Dictionary.py
+++ Lists/Dictionary.py
@@ -1,5 +1,6 @@
-#It is also a type of list in python which stores more than one values in a single variable .
-# in dictionary we can assign a key to a element , by using the key we can access the the  value associated with that particular key. 
-d = {'BA' :'Bhagyshree Ambulkar','DS' : 'Dhiraj Shembekar','RI':'Rekha Israni'}
-print(d['BA'])  #Accessing  the value by using key name i.e., BA, DS etc..
-print(d.keys()) #Accessing  all keys present inside dictionary 
+# It is also a type of list in python which stores more than one values in a single variable .
+# in dictionary we can assign a key to a element , by using the key we can access the the  value associated with that particular key.
+d = {"BA": "Bhagyshree Ambulkar", "DS": "Dhiraj Shembekar", "RI": "Rekha Israni"}
+print(d["BA"])  # Printing  the value by using key name i.e., BA, DS etc..
+print(d.keys())  # Printing  all keys present inside dictionary using Key() Method
+print(list(d.values()))    #  Printing all values present inside Dictionary using Values() Method
