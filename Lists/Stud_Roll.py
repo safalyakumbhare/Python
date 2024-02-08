bb3e5ec059ed0cc31162b9e85d654938fc995498
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-08 19:03:12 +0530

    Stud_Roll

---
+++ Lists/Stud_Roll.py
@@ -0,0 +1,4 @@
+#Creating a program to print the name of the student by inputing there Roll number 
+bcca = {1:"Abhay",2:"Abhinav",3:"Aditya",4:"Adnan",5:"Bhuvan",6:"Deekshansh",7:"Dhanajay"} 
+roll = int(input("Enter Your Roll Number : "))
+print ("Your Name is: ", bcca[roll])
