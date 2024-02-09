610b83f4cbafc4861d3616973f6f7ea1b5224007
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-09 23:02:08 +0530

    Dictionary List

---
+++ Lists/Dictionary Methods.py
@@ -0,0 +1,14 @@
+d = { "faculties" :{"BA": "Bhagyshree Ambulkar", "DS": "Dhiraj Shembekar", "RI": "Rekha Israni"},
+     "Deans" : {"SJ":"Sudhir Juare"}}
+
+print(
+    "The Value of the key BA is : ", (d["faculties"]["BA"])
+)  # Printing  the value by using key name i.e., BA, DS etc..
+print(
+    "The all keys are : ", d.keys()
+)  # Printing  all keys present inside dictionary using Key() Method
+print(
+    "THe ALL Values are : ", list(d.values())
+)  #  Printing all values present inside Dictionary using Values() Method
+print(list(d.items()));
+# Printing All items (Key-Value pairs) in a Dictionary using Items() method
