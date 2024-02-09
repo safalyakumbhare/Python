a58bb15579248f8044af30a2dbe13c82aa60221e
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-09 23:24:34 +0530

    Dictionary methods

--- Lists/Dictionary Methods.py
+++ Lists/Dictionary Methods.py
@@ -1,5 +1,4 @@
-d = { "faculties" :{"BA": "Bhagyshree Ambulkar", "DS": "Dhiraj Shembekar", "RI": "Rekha Israni"},
-     "Deans" : {"SJ":"Sudhir Juare"}}
+d = { "faculties" :{"BA": "Bhagyshree Ambulkar", "DS": "Dhiraj Shembekar", "RI": "Rekha Israni"},"SJ":"Sudhir Juare"}
 
 print(
     "The Value of the key BA is : ", (d["faculties"]["BA"])
@@ -12,3 +11,4 @@ print(
 )  #  Printing all values present inside Dictionary using Values() Method
 print(list(d.items()));
 # Printing All items (Key-Value pairs) in a Dictionary using Items() method
+print(d.get("LP"))   # If any  one provide key which is not available in the Dictionary it will return None otherwise It will print the corresponding Value for that particular Key.
