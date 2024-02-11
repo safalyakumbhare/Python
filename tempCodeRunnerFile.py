ff00ee69fd2ffa9cdff71e0ffc94a720a0c77161
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-11 19:01:07 +0530

    Temp

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,3 @@
-num = int(input("Enter a Number : "))
-if num % 7 ==0:
-    print("The Number is Divisible by 7")
-else:
-    print("The Number is Not Divisible")
+classroom = {"python","java","c++","python","javasript","java","python","java","c++","c"}
+
+print(len(classroom))
