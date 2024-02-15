96a044bdefd0f359345fcad921d147a465667fdf
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-15 23:16:12 +0530

    List Method

--- Lists/List Methods.py
+++ Lists/List Methods.py
@@ -6,5 +6,7 @@ num = [4,8,7,2,9,1,5,3,6,10]
 # num.reverse()  # Reverses the order of the elements in a list
 # num.insert(3,999) # Inserts an item to any position in the list
 # num.remove(7)    # Removes the specified value from the list
+# len = len(num)   # Returns the number of elements in the list
+
 for i in num:
     print(i)
