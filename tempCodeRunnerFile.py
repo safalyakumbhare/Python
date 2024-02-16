dc0e582c4f1691b8bb9df6d3da6b74d682891ec5
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-16 19:29:45 +0530

    Temp

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,7 @@
-classroom = {"python","java","c++","python","javasript","java","python","java","c++","c"}
-
-print(len(classroom))
+a = 100
+if a>10:
+    print("thik hai")
+elif a>20:
+    print("samajh gaya")
+elif a>30:
+    print("ok")
