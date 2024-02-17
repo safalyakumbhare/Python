21cc171434339ba77eff7ce8059ec465c5dfeb60
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-17 20:18:12 +0530

    Swapping the value

---
+++ swap.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the value of a"))
+b = int(input("Enter the value of b"))
+c = int(input("Enter the value of c"))
+d = int(input("Enter the value of d"))
+
+a,b,c,d = b,c,d,a
+print(a)
+print(b)
+print(c)
+print(d)
