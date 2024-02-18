fb5b3b1bfccb22166f93ee6c3e7c58dc69cc0543
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-18 23:48:45 +0530

    For Loop

--- Loops/for.py
+++ Loops/for.py
@@ -1,3 +1,10 @@
-#Print 1 to 50
-for i in range(1,51):
+print("Print 1 to 10")
+for i in range(1,11):
     print(i)
+
+print(" ")
+
+print("Print the list Items using for loop")
+num = [1,4,9,16,25,36,49,64,81,100]
+for n in (num):
+    print(n)
