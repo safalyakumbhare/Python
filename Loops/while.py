31c831e0c50ed09e33ffc0c90948d4bea966d093
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-18 23:48:33 +0530

    While Loop

--- Loops/while.py
+++ Loops/while.py
@@ -1,10 +1,10 @@
-#Print 1 to 50  in reverse order
+print("Print 1 to 50  in reverse order")
 i = 50
 while (i>=1):
     print(i)
     i-=1
 
-# Printing List Item using while loop
+print("Printing List Item using while loop")
 num = [1,4,9,16,25,36,49,64,81,100]
 i = 0
 while i<len(num):
