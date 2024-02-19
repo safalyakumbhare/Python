c6de4132406c3a7673664492ee7b0431e81e3784
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-19 20:45:58 +0530

    Break Statements

---
+++ Loops/break_statement.py
@@ -0,0 +1,6 @@
+#Create a program and run the loop from 1 to 20 but the execution must be stop at 15 ?
+for i in range (1,21):
+    print(i)
+    if i == 15:
+        break
+print("Execution Stopped")
