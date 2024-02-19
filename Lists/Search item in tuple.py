356dcd15e910137f58f2d717b242a5e4ea49ba24
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-19 19:17:03 +0530

    Search Item

--- Lists/Search item in tuple.py
+++ Lists/Search item in tuple.py
@@ -1,7 +1,11 @@
 num = (1,4,9,16,25,36,49,64,81,100)
 search = int(input("Enter any item from the tuple : "))
 i = 0
-while i < len(num):
-    if num[i] == search:
-        print("Found ",num[i]," at index", i)
-    i += 1
+# while i < len(num):
+#     if num[i] == search:
+#         print("Found ",num[i]," at index", i)
+#     i += 1
+
+for j in (1,len(num)-1):
+    if(num[j]== search):
+        print(f"The element {search} is present at index {j}")
