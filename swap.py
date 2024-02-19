4e141d436b9afb3b093380bc738c62c7a40df8fe
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-19 19:31:24 +0530

    Swap Values

--- swap.py
+++ swap.py
@@ -3,7 +3,14 @@ b = int(input("Enter the value of b"))
 c = int(input("Enter the value of c"))
 d = int(input("Enter the value of d"))
 
+# a = a+b+c+d
+# b = a-b-c-d
+# c = a-b-c-d
+# d = a-b-c-d
+# a = a-b-c-d
+
 a,b,c,d = b,c,d,a
+
 print(a)
 print(b)
 print(c)
