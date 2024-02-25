61bdcac0bced05704a6d6fd7aa8f8dc34d8418d8
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-26 00:07:16 +0530

    W Mode Write Data

--- File Handling/W Mode writing.py
+++ File Handling/W Mode writing.py
@@ -1,5 +1,5 @@
+#W Mode Opens the file to write the data and if the data id already exist then it will Delete the previous and write the new data.
 f = open("D:\All Programs\Python\File Handling\demo.txt","w")
-f.write("Hey! There I m writing a new statement")
-
+f.write("Hey there I m safalya here to introduce my self")
 
 f.close()
