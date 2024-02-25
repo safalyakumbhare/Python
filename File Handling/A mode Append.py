609595d1ca8e9b2ba43bf96933a856e65d90da24
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-26 00:07:04 +0530

    A Mode Append Data

--- File Handling/A mode Append.py
+++ File Handling/A mode Append.py
@@ -1,2 +1,5 @@
+#A Mode is used to add the data at the end of the content of the file and the previous will be as it is.
 f = open("D:\All Programs\Python\File Handling\demo.txt", "a")
-f.write("Which is more edited by the the A mode i.e Append Mode")
+f.write(
+    "\n I am a Web Developer with a great knowledge of  HTML and CSS Animations and effects."
+)
