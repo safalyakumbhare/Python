c17657e4bbba8a7c14b48e40240e5ce7c1aa6758
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-29 01:04:10 +0530

    Constructor in Python

---
+++ OOPs/Constructor_oops.py
@@ -0,0 +1,13 @@
+class op:
+    name = "op"
+    roll = 45
+    def __init__(self):
+        self.name 
+        self.roll 
+    
+    def stud(self):
+        print("Your Name :" ,self.name)
+        print("Your Name :" ,self.roll)
+
+obj = op()
+obj.stud()
