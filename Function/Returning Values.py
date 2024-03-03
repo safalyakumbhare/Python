819d7e024ec0c2c1c35a0ab5411fa7f2524c6081
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-03-03 19:26:53 +0530

    Returning Values

---
+++ Function/Returning Values.py
@@ -0,0 +1,6 @@
+# Return Keyword is used to return a  value from the function and at the time of calling the function we must call it within the print statement
+def add(a, b):
+    return a + b
+
+
+print(add(1, 2))
