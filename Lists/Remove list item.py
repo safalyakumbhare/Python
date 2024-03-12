ae596168b24df2c0ba7fac28aa22eeef7a662a16
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-03-13 00:27:53 +0530

    Remove item from list

---
+++ Lists/Remove list item.py
@@ -0,0 +1,5 @@
+comp = ['Microsoft', 'Google', 'Nwedia', 'Qualcomm', 'Netflix', 'Facebook', 'Infosys', 'Xiaomi', 'Samsung'] 
+item = input("Enter the item to Remove :")
+comp.remove(item)
+
+print(comp)
