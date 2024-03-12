510c28f00d792c813f2f54e1bf7bfdde9e289817
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-03-13 00:22:39 +0530

    User input List

--- Lists/User input list.py
+++ Lists/User input list.py
@@ -1,5 +1,8 @@
-names = []
-names.append(input("Enter First Movie Name : "))
-names.append(input("Enter Second Movie Name : "))
-names.append(input("Enter Third Movie Name : "))
-print(names)
+company = []
+n = int(input("Enter the number of list items you entering : "))
+
+for i in range(1, n+1):
+    company.append(input("Enter Company Names : "))
+
+
+print(company)
