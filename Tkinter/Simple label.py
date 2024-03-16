d78dfc144dcfa68d4d43c2bbff15bc07a690b870
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-03-17 01:17:26 +0530

    Label through click

--- Tkinter/Simple label.py
+++ Tkinter/Simple label.py
@@ -1,6 +1,15 @@
 from tkinter import *
-w= Tk()
-l = Label(w,text="Hello Python GUI")
+
+w = Tk()
+l = Label(w)
+
+
+def click():
+    l.config(text="Click ho gaya yeee!")
+
+
+b = Button(w, text="Submit", command=click)
 l.pack(padx=12)
-w.geometry('300x300')
+b.pack(padx=30)
+w.geometry("300x300")
 w.mainloop()
