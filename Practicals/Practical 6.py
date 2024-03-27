661e0f87da9b8ee2f37d0573c75381dd59e1c5b1
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-03-27 16:04:27 +0530

    Practical

--- Practicals/Practical 6.py
+++ Practicals/Practical 6.py
@@ -1,6 +1,9 @@
-file = open("newfile","a+")
+file = open("newfile","w")
 file.write("This is the New file")
+
+fileappend = open("newfile","a")
 file.write(" and writing more data using append.")
+
 fileread = open("newfile","r")
 data = fileread.read()
 print(data)
