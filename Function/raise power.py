0230d9a17e536ed8a2711f8d42d1ce1cdd5ebb86
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-05-06 13:02:10 +0530

    Raise to Power function

---
+++ Function/raise power.py
@@ -0,0 +1,8 @@
+def rai(x, y):
+    for i in range(y):
+        sum = x**y
+    print(x, " Raised to power ", y, " is ", sum)
+
+x = int(input("Enter the value of x "))
+y = int(input("Enter the value of y "))
+rai(x, y)
