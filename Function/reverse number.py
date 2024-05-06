b97d25d5eb2788a35f8bf48651ea276e55452c1e
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-05-06 13:18:09 +0530

    Reverse number

---
+++ Function/reverse number.py
@@ -0,0 +1,10 @@
+def rev(num):
+    rvs = 0
+    while (num > 0):
+        dig = num % 10
+        rvs = (rvs*10)+dig
+        num = num//10
+    print("The reversed Number are ",rvs)
+
+num = int(input("Enter the number : "))
+rev(num)
