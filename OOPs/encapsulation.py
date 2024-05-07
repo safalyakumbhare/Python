943f3f2b630e6c01e5c5122f7e31296f32f1692b
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-05-07 13:52:25 +0530

    Encapsulation

--- OOPs/encapsulation.py
+++ OOPs/encapsulation.py
@@ -2,14 +2,13 @@ class A:
     _a = 10   #protected variable
     __b = 20  #private variable
     def show(self):
-        print("a = :", self._a)
-        self._a = 11
-        print("a = :", self._a)
-        self.__b = 22
-        print("b = :", self.__b)
+        print("Inside the class a = :", self._a)
+        print("Inside the class b = :", self.__b)
 
 
 obj = A()
 obj.show()
-print(A._a)
-# print(A.__b)
+A._a = 11
+A.__b = 22
+print("Outside the class a = ",A._a)
+print("Outside the class b = ",A.__b)
